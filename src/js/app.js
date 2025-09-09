c5d300d82762974fc5d84857371214d9e01180cc
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2025-09-09 19:57:29 +0300

    app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 export function getHealthStatus(character) {
   if (character.health > 50) return 'healthy';
   if (character.health >= 15) return 'wounded';
-  return 'critica';
+  return 'critical';
 }
 
 export function sortHeroesByHealth(heroes) {
